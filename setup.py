e3add048f4ffde4ba084fd931ce963a0420ea5a0
Adam Georgiou <adamgeorgiou@google.com>
2015-12-08 08:44:55 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mgrok',
+    version = '0.1',
+    author = 'Adam Georgiou',
+    author_email = 'me@adamgeorgiou.com',
+    description = ('Utility to grok concerts'),
+    packages=find_packages('src'),
+    package_dir = {'':'src'}
+)
