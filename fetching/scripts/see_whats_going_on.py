b8d4f85a18185f9b6cb36a61c1ae6bf2fe9eeb63
Adam Georgiou <me@adamgeorgiou.com>
2015-12-27 22:29:35 -0500

    Moved api fetching stuff into its own class. added some styling

--- fetching/scripts/see_whats_going_on.py
+++ fetching/scripts/see_whats_going_on.py
@@ -6,10 +6,7 @@ into a single output file.
 """
 
 import json
-import os
 import sys
-from datetime import date, datetime, timedelta
-from operator import attrgetter
 
 from mgrok.scrapers import (
     BoweryBallroomSpider,
@@ -20,27 +17,14 @@ from mgrok.scrapers import (
     HighlineBallroomSpider,
     WarsawSpider
     )
+from mgrok.ticketfly_api import (
+    BrooklynBowlApi,
+    CapitolTheatreApi
+    )
 from dateutil.parser import parse as parse_date_str
-import pytz
-import requests
 from scrapy.crawler import CrawlerProcess
 from scrapy.settings import Settings
 
-TICKETFLY_DAYS_BEHIND = 7
-
-TICKETFLY_DAYS_AHEAD = 3 * 31
-
-TICKETFLY_API_BASE_URL = 'http://www.ticketfly.com/api'
-
-TICKETFLY_API_VENUES = {
-    'Brooklyn Bowl': '1',
-    'Capitol Theatre': '4725'
-    }
-
-TICKETFLY_API_ORGS = {
-    'Brooklyn Bowl': '3',
-    'Capitol Theatre': '767'
-    }
 
 SCRAPY_SPIDERS = [
     WarsawSpider,
@@ -84,75 +68,11 @@ def get_scraped_sites_data():
 
 
 def get_api_sites_data():
-    """Returns output for venues which provide api access."""
-
-    def make_request(page_num=1):
-        """Returns the response from querying the ticketfly api."""
-        list_event_endpoint = os.path.join(
-            TICKETFLY_API_BASE_URL, "events/list")
-        from_date = (
-            date.today() -
-            timedelta(TICKETFLY_DAYS_BEHIND)).strftime("%Y-%m-%d 00:00:00")
-        thru_date = (
-            date.today() +
-            timedelta(TICKETFLY_DAYS_AHEAD)).strftime("%Y-%m-%d 23:59:59")
-        request_params = {
-            'maxResults': 1000,
-            'venueId': ','.join(TICKETFLY_API_VENUES.values()),
-            'orgId': ','.join(TICKETFLY_API_ORGS.values()),
-            'fromDate': from_date,
-            'thruDate': thru_date,
-            'pageNum': page_num,
-            }
-
-        return requests.get(list_event_endpoint, params=request_params)
-
-    def filter_events(events):
-        """
-        Filter API-returned event objects into more concise application-specific
-        event objects
-        """
-        filtered_events = {}
-        for event in events:
-            artists = []
-            for headliner in event['headliners']:
-                artists.append(headliner['name'])
-            for supporter in event['supports']:
-                artists.append(supporter['name'])
-            venue_name = event['venue']['name']
-
-            time_zone = pytz.timezone(event['venue']['timeZone'])
-            event_date = time_zone.localize(
-                datetime.strptime(event['startDate'], '%Y-%m-%d %H:%M:%S'))
-            url = event['ticketPurchaseUrl']
-
-            if venue_name not in filtered_events:
-                filtered_events[venue_name] = []
-
-            filtered_events[venue_name].append({
-                'artists': artists,
-                'venue_name': venue_name,
-                'date': event_date.isoformat(),
-                'event_link': url
-                })
-
-        return filtered_events
-
-    # Collect all event data
-    response = make_request()
-    events = []
-    current_page = response.json()['pageNum']
-    total_pages = response.json()['totalPages']
-    while True:
-        events.extend(response.json()['events'])
-        next_page = current_page + 1
-        if next_page > total_pages:
-            break
-        response = make_request(next_page)
-        current_page = next_page
-
-    return filter_events(events)
-
+    """Returns output for venues which have APIs"""
+    output = {}
+    output.update(BrooklynBowlApi().get_api_sites_data())
+    output.update(CapitolTheatreApi().get_api_sites_data())
+    return output
 
 def main():
     """
@@ -170,7 +90,7 @@ def main():
     def key_function(event):
         date_str = event['date']
         return parse_date_str(date_str)
-    for venue, events in shows.iteritems():
+    for events in shows.values():
         events.sort(key=key_function)
 
     # Print shows
---
+++ fetching/src/mgrok/ticketfly_api.py
@@ -0,0 +1,117 @@
+"""
+Classes for collecting venue information from the venues who use TicketFly
+"""
+
+from datetime import date, datetime, timedelta
+import os
+import re
+
+import requests
+import pytz
+
+class _TicketFlyApi(object):
+    """
+    Base class for fetching data from ticketfly api.
+    """
+    base_url = 'http://www.ticketfly.com/api'
+    days_behind = 7
+    days_ahead = 3 * 31
+
+    def __init__(self, venue_id, org_id):
+        self.venue_id = venue_id
+        self.org_id = org_id
+
+    def make_request(self, page_num=1):
+        """Returns the response from querying the ticketfly api."""
+        list_event_endpoint = os.path.join(self.base_url, "events/list")
+        from_date = (
+            date.today() -
+            timedelta(self.days_behind)).strftime("%Y-%m-%d 00:00:00")
+        thru_date = (
+            date.today() +
+            timedelta(self.days_ahead)).strftime("%Y-%m-%d 23:59:59")
+        request_params = {
+            'maxResults': 1000,
+            'venueId': self.venue_id,
+            'orgId': self.org_id,
+            'fromDate': from_date,
+            'thruDate': thru_date,
+            'pageNum': page_num,
+            }
+
+        return requests.get(list_event_endpoint, params=request_params)
+
+    def format_events(self, events):
+        """
+        Filter API-returned event objects into more concise application-specific
+        event objects
+        """
+        formatted_events = {}
+        for event in events:
+            artists = []
+            for headliner in event['headliners']:
+                artists.append(headliner['name'])
+            for supporter in event['supports']:
+                artists.append(supporter['name'])
+            venue_name = event['venue']['name']
+
+            time_zone = pytz.timezone(event['venue']['timeZone'])
+            event_date = time_zone.localize(
+                datetime.strptime(event['startDate'], '%Y-%m-%d %H:%M:%S'))
+            url = event['ticketPurchaseUrl']
+
+            if venue_name not in formatted_events:
+                formatted_events[venue_name] = []
+
+            formatted_events[venue_name].append({
+                'artists': artists,
+                'venue_name': venue_name,
+                'date': event_date.isoformat(),
+                'event_link': url
+                })
+
+
+        for venue_name, event_list in formatted_events.iteritems():
+            formatted_events[venue_name] = (
+                self.venue_specific_filter(event_list))
+        return formatted_events
+
+    def venue_specific_filter(self, events):
+        return events
+
+    def get_api_sites_data(self):
+        response = self.make_request()
+        events = []
+        current_page = response.json()['pageNum']
+        total_pages = response.json()['totalPages']
+        while True:
+            events.extend(response.json()['events'])
+            next_page = current_page + 1
+            if next_page > total_pages:
+                break
+            response = self.make_request(next_page)
+            current_page = next_page
+
+        return self.format_events(events)
+
+
+class BrooklynBowlApi(_TicketFlyApi):
+    """Fetches data for events at Brooklyn Bowl"""
+    def __init__(self):
+        super(BrooklynBowlApi, self).__init__(1, 3)
+
+    def venue_specific_filter(self, events):
+        def filter_family_bowl(event):
+            for artist in event['artists']:
+                regex = re.compile(r'.*family bowl.*', re.IGNORECASE)
+                if re.match(regex, artist):
+                    return False
+            return True
+
+        return filter(filter_family_bowl, events)
+
+
+class CapitolTheatreApi(_TicketFlyApi):
+    """Fetches data for events at The Capitol Theatre"""
+    def __init__(self):
+        super(CapitolTheatreApi, self).__init__(4725, 767)
